24059652df153f4985f322ee353fe1ced69c300d
w-createira <w.ira.verbilovich@gmail.com>
2025-08-25 16:32:14 +0300

    feat: Add logic to addition button

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,15 @@
-alert('This workd!');
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
+function add() {
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} + ${enteredNumber}`;
+    currentResult = currentResult + enteredNumber;
+    outputResult(currentResult, calcDescription);
+}
+
+addBtn.addEventListener('click', add);
